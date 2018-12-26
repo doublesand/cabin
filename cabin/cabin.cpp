16e5c3e4e05ebee5f3d55420be5c51b4147af570
doublesand <doublesand@163.com>
2018-12-26 12:55:57 +0800

    尝试不同的环绕方式，超过纹理坐标后的线性填充方式

--- cabin/cabin.cpp
+++ cabin/cabin.cpp
@@ -49,10 +49,10 @@ int main()
 
 	float vertices[] = {  //顶点一般有坐标、颜色、纹理、法向量，这里已经有三个了，以后继续添加
 		// positions          // colors           // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f, // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f  // top left 
 	};
 	
 
